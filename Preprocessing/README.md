59e72b7e0826d1494568aefc2bae58ccd1ddcbea
jzulua70 <jzulua70@eafit.edu.co>
2019-03-07 16:50:53 -0500

    adding two new files, cleanStructurer(cleans all the data folder about json files problems and empty rgb folders into the lots folders) and dataStructuring(splits the data into train_set and test_set)

--- Preprocessing/README.md
+++ Preprocessing/README.md
@@ -2,6 +2,7 @@
 
 This part of project is focused on tries to make a cleaning of the diferent types of data(re,rgn,rgb and json) using image processing and machine learning algorithms, with the final intention to pass them to a deep learning model and get better accuracy and behavior.
 
+
 # Sources Files:
 
 1. createStructure:
@@ -64,12 +65,59 @@ The output is the xml file which can be opened with an ethernet navigator.
 
 
 
+4. cleanStructure:
+
+This code is the first part of the pipeline and it cleans all the data folder about json files problems and empty rgb folders into the lots
+
+Usage:
+
+- python3.6 cleanStructure.py -fp --Folder where the data
+
+Output: data folder without bad json files and empty rgb folder. 
 
 
+Functions table:
 
+
+                                     cleanStructure.py
+|Function       |     Input parameters    |                 Return values                        |
+|---------------|--------------------------|-----------------------------------------------------|
+|findTypes   |     folder(Str)       | 	   	 													 |
+|cleanJson  | json_path(str)              | document{}(dictionary))   	 		                 |
+|findJson  | elements[](str)              | jsonFile(str)   	 		                 |
+|amountOfFiles  | data_path(str)              | cont(int)   	 		                 |
+|findRgbFolder  | elements[](str)              | band(bool)   	 		                 |
+
+The explanation of each function can be find inside of the source code.
+
+
+5. dataStructuring
 		
 
- 	
+This code splits the data into train_set and test_set through a stop criterion where test is representated by the 25% of the minimum of the amount of the any type (json,rgb,re,rgn)
+
+Usage:
+
+- python3.6 dataStructuring.py -fp --Folder where is the data -fd --The name of the new folder where is going to be the data
+
+
+Output: New folder which contains two folders, train_set and test_set 
+
+
+Functions table:
+
+
+                                     dataStructuring.py
+|Function       |     Input parameters    |                 Return values                        |
+|---------------|--------------------------|-----------------------------------------------------|
+|run -> main method   |          | 	   	 													 |
+|validate_stop_criterion  | lot_directory(str), type_selection(str)              | cont_type(int)   	 		                 |
+|valid_for_test  | lot_directory(str)              | band(bool)   	 		                 |
+|get_stop_criterion  | list_conts[](str)              | stop_criterion(int),type_selection(str)   	 		                 |
+|elements_numbers_for_type  | data_path(str)              | cont_json(int), cont_rgb(int), cont_rgn(int), cont_re(int)   	 		                 |
+
+The explanation of each function can be find inside of the source code.
+
 
 
 
---
+++ Preprocessing/cleanStructure.py
@@ -0,0 +1,183 @@
+import cv2
+import os
+import argparse
+from multiprocessing import Pool
+import multiprocessing
+import shutil
+import numpy as np
+import imutils
+import dlib
+import json
+import glob
+import shutil
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-fp", "--fp", help="Folder where is the data")
+args = parser.parse_args()
+
+
+class preprocessData:
+
+	global file, folderPath
+	def __init__(self,folderPath):
+		
+		self.folderPath = folderPath
+		
+	def findRgbFolder(self,list_elements):
+		"""
+			Finds if there is a rgb folder
+		"""
+
+		for l in list_elements:
+			if l.find("rgb_images") != -1:
+				return True
+
+		return False
+
+	def amountOfFiles(self, data_path):
+		"""
+		 	Gets the total number of files into the folder path
+		"""
+		cont = 0
+		for root, dirs, files in os.walk(data_path):
+			for file in files:
+				cont +=1
+
+		return cont
+		
+	def findJson(self, elements):
+
+		"""
+			Finds which element is the json file
+		"""
+		jsonFile = ""
+		for e in elements:
+			if e.find(".json") != -1:
+				jsonFile = e
+
+
+		return jsonFile
+
+	def findTypes(self,folder):
+
+		"""
+			Search in the given folder for the four types and classify them into a differents lists
+		"""
+		print("Cleaning folder...")
+		lot_dirs = list()
+		for root, dirs, files in os.walk(folder):
+			
+			# Iterates over the first layer of directories.
+			for directory in dirs:
+				directory_path = os.path.join(root, directory)	
+				# Iterates over each element within the directory path.
+				for lot_directory in os.listdir(directory_path):
+					lot_directory_path = os.path.join(directory_path, lot_directory)
+					# Appends the current element to the list if it is a directory.
+					lot_dirs.append(lot_directory_path) if os.path.isdir(lot_directory_path) else None
+			# Only the first layer of directories is required. It is not necessary to keep walking through the tree.
+			break
+
+		# Iterates over all lot folders
+		for lot in lot_dirs:
+			# Gets the elements of the lot folder
+			elements = glob.glob(lot+"/*")
+			
+			# If there is not a rgb folder inside, remove the lot folder
+			if self.findRgbFolder(elements):
+				
+				# Gets the json file 
+				jsonFile = self.findJson(elements)
+
+				# Cleans the json
+				document = self.cleanJson(jsonFile)
+				
+				# Checks if Json is ok
+				if len(document) == 0:
+
+					if jsonFile != "":
+						os.remove(jsonFile)
+
+					# Gets how many files are into the lot folder
+					amountOfFiles = self.amountOfFiles(lot)
+
+					# If there are not files
+					if amountOfFiles == 0:
+						shutil.rmtree(lot)
+
+			else: 
+				shutil.rmtree(lot)
+
+		print("Folder cleaned")
+				
+			
+	def cleanJson(self, json_path):
+
+		try:
+			with open(json_path) as f:
+				
+				if os.stat(json_path).st_size == 0:
+					return {}
+
+				data = json.load(f)
+				
+				# validate if all the keys exists
+				if 'ph' not in data or 'soil_temperature' not in data or 'soil_moisture' not in data or 'illuminance' not in data or 'env_temperature' not in data or 'env_humidity' not in data:
+					return {}
+
+				# validate the ranges of the values
+				band = True
+				ph = data['ph']
+				if ph < 4.5 or ph > 9.5:
+					band = False
+
+				s_temp = data['soil_temperature']
+				if s_temp > 40:
+					band = False
+
+				s_moist = data['soil_moisture']
+				if s_moist < 10 or s_moist > 95:
+					band = False
+
+				illuminance = data['illuminance']
+				if illuminance < 40 or illuminance > 15000:
+					band = False
+
+				env_temp = data['env_temperature']
+				if env_temp < 10 or env_temp > 40:
+					band = False
+
+				env_humi = data['env_humidity']
+				if env_humi < 20 or env_humi > 99:
+					band = False
+				document = {}
+				# If all the ranges are good save them
+				if band:
+				
+					document = {
+						'ph' : ph,
+						'soil_temperature' : s_temp,
+						'soil_moisture': s_moist,
+						'illuminance' : illuminance,
+						'env_temperature' : env_temp,
+						'env_humidity' : env_humi
+						} 
+					
+				else:		
+			
+					document = {}
+
+				return document
+				
+		
+		except:
+			 return {}
+
+			
+p = preprocessData(args.fp)
+p.findTypes(args.fp)
+
+
+
+
+
